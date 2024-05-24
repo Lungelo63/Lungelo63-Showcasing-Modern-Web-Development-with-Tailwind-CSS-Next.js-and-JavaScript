8383a56c0c8a350a6e8b8867fbbad9dca7aecf5a
Lungelo <mbalipackery@gmail.com>
2024-05-24 22:32:15 +0200

    Progress since last push

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque, Oswald } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import GrainEffect from "@/components/visualEffects/grain-effect";
 
 //Main font used
 const MainFont = Bricolage_Grotesque({ subsets: ["latin"] });
@@ -32,7 +33,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(MainFont.className, OswaldFont.variable, PixelFont.variable)}>{children}</body>
+      <body 
+        className={cn(
+          MainFont.className, 
+          OswaldFont.variable, 
+          PixelFont.variable
+        )}
+      >
+        <GrainEffect />
+        {children}
+      </body>
     </html>
   );
 }
