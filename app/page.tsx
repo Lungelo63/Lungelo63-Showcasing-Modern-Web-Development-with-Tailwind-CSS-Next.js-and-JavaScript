394a381c449ec7844228427e97a118e8f63f7db8
Lungelo <mbalipackery@gmail.com>
2025-07-14 02:41:25 +0200

    New updates to the existing web portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 "use client"
+import Button from "@/components/visualEffects/ui/button";
+import Input from "@/components/visualEffects/ui/input";
 import Image from "next/image";
 import clsx from "clsx";
 import { cn } from "@/lib/utils";
+import { FaArrowRight, FaRegUser } from "react-icons/fa6";
+import { AiOutlineHome } from "react-icons/ai";
 //import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 import Card from "@/components/visualEffects/ui/card";
 
 import dynamic from "next/dynamic";
+import TextArea from "@/components/visualEffects/ui/text-area";
+import FancyButton from "@/components/visualEffects/ui/fancy-button";
 // Dynamic import to make sure it is only used in the client side env
 const WaterWaveWrapper = dynamic (
   () => import("@/components/visualEffects/water-wave-wrapper"),
@@ -23,10 +29,30 @@ export default function Home() {
     {() => ( 
       <div className="w-full p-10">
         <div className="max-w-2xl mx-auto">
-          <Card title="Ui components">...</Card>
+          <Card title="Ui components">
+            <div className="grid grid-cols-4">
+              <Button>Basic Button</Button>
+              <Button>
+                <AiOutlineHome/>
+                Basic Button
+                </Button>
+                <Button isIcon>
+                  <FaRegUser/>
+                  </Button>
+                  <Button link="https://www.google.com">Google</Button>
+            </div>
+            <Input type="text" placeholder="Full name"/>
+            <Input type="text" placeholder="Full name" icon={<FaRegUser />} />
+            <TextArea placeholder="Full name"  />
+            <TextArea placeholder="Full name" icon={<FaRegUser />} />
+            <div className="w-[320px]">
+            <FancyButton text="Contact me" icon={<FaArrowRight/>} />
+            </div>
+          </Card>
         </div>
       </div>
     )}
     </WaterWaveWrapper>
   );
 }
+
